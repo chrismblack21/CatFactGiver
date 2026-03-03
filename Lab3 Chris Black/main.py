346ea3919a73e13a073a7f2aff757bc4a503702c
chrismblack21 <chrismblack21@gmail.com>
2026-03-02 23:22:55 -0500

    Add files via upload

--- Lab3 Chris Black/main.py
+++ Lab3 Chris Black/main.py
@@ -12,17 +12,34 @@ def get_random_fact():
     print("Random Cat Fact:")
     print(data['fact'])
 
+def get_multiple_facts():
+    limit = input("How many cat facts would you like? ")
+    url = f"{BASE_URL}/facts?limit={limit}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    print("\nCat Facts:")
+    for fact in data['data']:
+        print(f"- {fact['fact']}")
+
+# main menu function
 def main():
     while True:
         print("\nMenu:")
         print("1. Get a random cat fact")
-        print("2. Exit")
+        print("2. Get multiple cat facts")
+        print("3. Exit")
 
         choice = input("Enter your choice: ")
 
         if choice == '1':
             get_random_fact()
+            
         elif choice == '2':
+            get_multiple_facts()
+
+        elif choice == '3':
             print("Goodbye.")
             break
         else:
