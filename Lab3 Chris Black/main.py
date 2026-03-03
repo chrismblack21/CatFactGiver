49bcb5fbbc3980dc4dde081f54f25787df2f565d
chrismblack21 <chrismblack21@gmail.com>
2026-03-02 23:32:05 -0500

    Add files via upload
    finished product

--- Lab3 Chris Black/main.py
+++ Lab3 Chris Black/main.py
@@ -12,6 +12,7 @@ def get_random_fact():
     print("Random Cat Fact:")
     print(data['fact'])
 
+# function to get multiple cat facts based on user input
 def get_multiple_facts():
     limit = input("How many cat facts would you like? ")
     url = f"{BASE_URL}/facts?limit={limit}"
@@ -23,13 +24,24 @@ def get_multiple_facts():
     for fact in data['data']:
         print(f"- {fact['fact']}")
 
+# function to get cat breeds. 
+def get_cat_breeds():
+    url = f"{BASE_URL}/breeds"
+    response = requests.get(url)
+    data = response.json()
+
+    print("\nCat Breeds:")
+    for breed in data['data']:
+        print(f"- {breed['breed']}")
+
 # main menu function
 def main():
     while True:
         print("\nMenu:")
         print("1. Get a random cat fact")
         print("2. Get multiple cat facts")
-        print("3. Exit")
+        print("3. Get cat breeds")
+        print("4. Exit")
 
         choice = input("Enter your choice: ")
 
@@ -40,6 +52,9 @@ def main():
             get_multiple_facts()
 
         elif choice == '3':
+            get_cat_breeds()
+
+        elif choice == '4':
             print("Goodbye.")
             break
         else:
